0544060bd89f7b2fd25f1113f53fd0d1d1eb0516
bearddan2000 <bearddan2000@yahoo.com>
2024-10-26 18:00:35 -0400

    initial commit

--- bin/src/main/java/example/Main.java
+++ bin/src/main/java/example/Main.java
@@ -16,21 +16,7 @@ public class Main {
             .post("/basic-route-example", ctx -> ctx.text("POST called"))
             .put("/basic-route-example", ctx -> ctx.text("PUT called"))
             .delete("/basic-route-example", ctx -> ctx.text("DELETE called"))
-            .addStatics("/custom-static")
-            // .showFileList(true)
             .enableCors(true)
-            .before("/user/*", ctx -> log.info("[NarrowedHook] Before '/user/*', URL called: " + ctx.uri()))
-            .on(EventType.SERVER_STARTED, e -> {
-                String version = WebContext.blade()
-                    .env("app.version")
-                    .orElse("N/D");
-                log.info("[Event::serverStarted] Loading 'app.version' from configuration, value: " + version);
-            })
-            .on(EventType.SESSION_CREATED, e -> {
-                Session session = (Session) e.attribute("session");
-                session.attribute("mySessionValue", "Baeldung");
-            })
-            .use(new MyMiddleware())
             .start(Main.class, args);
     }
 }
